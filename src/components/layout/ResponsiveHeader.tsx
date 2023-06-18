d5aaf4c435906bb1a7f4c4167b997d8da3c5dc54
Naufal Rafiawan Basara <naufalbasara@outlook.com>
2023-06-18 21:57:58 +0700

    update: update index page responsiveness

--- src/components/layout/ResponsiveHeader.tsx
+++ src/components/layout/ResponsiveHeader.tsx
@@ -27,8 +27,9 @@ export default function Example() {
  
   const navList = (
     <ul className="mb-4 mt-2 flex flex-col gap-2 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6 ">
-      {links.map((link) => (
+      {links.map((link, index) => (
       <Typography
+        key={index}
         as="li"
         variant="small"
         color="blue-gray"
