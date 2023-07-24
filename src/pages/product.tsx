b6303780a24dcaceb28e4608bdc6e25a8b050245
Naufal Rafiawan Basara <naufalbasara@outlook.com>
2023-07-24 14:11:35 +0700

    fix: minor changes in product page

--- src/pages/product.tsx
+++ src/pages/product.tsx
@@ -6,15 +6,18 @@ import ProductCard from '@/components/ProductCard';
 export default function Product() {
   const [products, setProducts] = React.useState([{id: null, name:'', category:'', description:'', image_path: ''}])
   const dataFetchedRef = React.useRef(false);
+  const [isLoading, setLoading] = React.useState(false)
 
   React.useEffect(() => {
     if (dataFetchedRef.current) return;
     dataFetchedRef.current = true;
     try {
+      setLoading(true)
       fetch('api/products').then((response:any) => {
         response.json().then((response:any)=> {
           const data = [...response]
           setProducts(data);
+          setLoading(false)
         })
       })
     } catch {
@@ -28,16 +31,16 @@ export default function Product() {
       <main className='min-h-screen pt-32 flex flex-col justify-center items-center w-screen'>
         <h1 className='underline decoration-red-500 decoration-4'>Let's see our products</h1>
         <section className='sm:grid grid-cols-2 md:grid-cols-3 xl:grid-cols-4 my-8'>
-          {products?.map((obj):any => (
-            <ProductCard
-              key={obj.id}
-              name={obj.name}
-              category={obj.category}
-              description={obj.description}
-              image_path={obj.image_path}
-              href='#'
-            />
-            ))}
+          {!isLoading && products?.map((obj):any => (
+          <ProductCard
+            key={obj.id}
+            name={obj.name}
+            category={obj.category}
+            description={obj.description}
+            image_path={obj.image_path}
+            href='#'
+          />
+          ))}
         </section>
       </main>
     </Layout>
