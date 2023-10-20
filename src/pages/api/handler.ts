7445ed75a5c99d1e45997cad0f71b34b3c0b079e
naufalbasara <naufalbasara@outlook.com>
2023-10-20 23:12:53 +0700

    update: sending string dtype to thinger.io API

---
+++ src/pages/api/handler.ts
@@ -0,0 +1,8 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method == 'POST') {
+    res.status(200).json({name: "Bambang"});
+  }
+  res.status(200).json({ name: 'Bambang' });
+}
--- src/pages/project.tsx
+++ src/pages/project.tsx
@@ -6,20 +6,20 @@ import axios from 'axios';
 
 export default function Project() {
   const [text, SetText] = React.useState('');
-  const [powerState, setPowerState] = React.useState(false);
+  const [powerState, setPowerState] = React.useState("");
   const component = [1, 2, 3, 4, 5, 6, 7, 8, 9, '*', 0, '#']
 
   const submitState = async () => {
-    const response = await fetch('https://backend.thinger.io/v3/users/rtaufiqulrtrh/devices/Vector/resources/D0', {
+    const response = await fetch('https://backend.thinger.io/v3/users/rtaufiqulrtrh/devices/Vector/resources/stringResource', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
         'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkZXYiOiJWZWN0b3IiLCJpYXQiOjE1OTIyOTEwMjMsImp0aSI6IjVlZTg2ZWNmYTcyNmE5NjU3YTc3NjY4MiIsInVzciI6InJ0YXVmaXF1bHJ0cmgifQ.QdvuykrWxQ3at81w0VjL1eyfZ0dMzzGI9AvYB4cBfFg',
         'Accept': 'application/json'
       },
-      body: `${powerState}`
+      body: JSON.stringify({'value': `${text}`})
     })
-    setPowerState(!powerState)
+    // setPowerState(!powerState)
     console.log(response)
   }
 
@@ -36,9 +36,9 @@ export default function Project() {
             }
           </div>
           <div className='m-4 w-full flex justify-center items-center h-20 bg-gray-400'>
-            <input type='text' value={text}></input>
+            <input type='text' onChange={() => SetText(text)} value={text}></input>
             <Button className='bg-red-500 hover:bg-red-800 mx-6' onClick={() => {SetText('')}}>Delete</Button>
-            <Button className='bg-[#2CA87F] hover:bg-gray-500' onClick={() => {alert("Token Submitted")}}>Submit</Button>
+            <Button className='bg-[#2CA87F] hover:bg-gray-500' onClick={submitState}>Submit</Button>
           </div>
           
           <div className='container grid grid-cols-3 w-max'>
